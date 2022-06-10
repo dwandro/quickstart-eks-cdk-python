98ee17f5e1087466f0c6f070b3b85ac39961cfe1
wq_admin_iam-at-425812718530 <wqqiang@5c52309d3362.ant.amazon.com>
2022-06-10 17:26:44 +0800

    add rds connect parameter

--- cluster-bootstrap/eks_cluster.py
+++ cluster-bootstrap/eks_cluster.py
@@ -1929,8 +1929,7 @@ class EKSClusterStack(Stack):
                 cloudwatch_logs_exports=["error", "general", "slowquery"],
                 deletion_protection=False,
                 enable_performance_insights=True,
-                delete_automated_backups=True,
-                CfnDBSecurityGroup=cfn_dBSecurity_group
+                delete_automated_backups=True
                 # ,
                 # vpc_security_groups=rds_security_group
                 # backup_retention=core.Duration.days(1),
